8e17baa169533901b3031f9b63cc2bf71ecc1c33
greg archer <nihlton@gmail.com>
2020-05-19 13:29:53 -0700

    Updates

---
+++ precache-manifest.7a9e68b4659d569535af60da9e64a701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f096b0e46abb3f0e0e1a5df684c2da",
+    "url": "/simple-stop-watch/index.html"
+  },
+  {
+    "revision": "501bddd292439a3f6445",
+    "url": "/simple-stop-watch/static/css/main.bb4a2f53.chunk.css"
+  },
+  {
+    "revision": "0dfaf6b7d2c506e58f27",
+    "url": "/simple-stop-watch/static/js/2.46baf53c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-stop-watch/static/js/2.46baf53c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "501bddd292439a3f6445",
+    "url": "/simple-stop-watch/static/js/main.c64b4f0b.chunk.js"
+  },
+  {
+    "revision": "6816415af3a41a88f91d",
+    "url": "/simple-stop-watch/static/js/runtime-main.77afbde6.js"
+  }
+]);
